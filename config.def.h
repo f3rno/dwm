e4ed2efdcfa498c2e0938774af4e7bd2a2f58148
Cris Mihalache <me@f3rno.com>
2020-03-11 23:45:16 +0700

    (tweak) add default tag mappings for tg/slack, mv ff

--- config.def.h
+++ config.def.h
@@ -27,8 +27,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Telegram", NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Slack",    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
